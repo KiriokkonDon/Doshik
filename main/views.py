e42956f24a3fbd8556751180ddd3dcad434d0031
kiriokkon <akane.vip@mail.ru>
2022-04-04 23:20:48 +1000

    webapp

--- main/views.py
+++ main/views.py
@@ -13,22 +13,6 @@ def index(request):
     return render(request, 'main/index.html', data)
 
 def about(request):
-    Ff = {
-        'title': 'Стихи',
-        'obj': {
-            'С':"Рисуй меня пеплом наброском на камне, "
-                "Обмытый водой взор покинутых душ, "
-                "Будто моря плещут между морями, "
-                "Теряя из виду мольберты из суш. "
-                "пусть краски текут, "
-                "создают новые линии; "
-                "Липкие и вязким телом "
-                "растушуй диссонанс "
-                "выходить из воды, когда "
-                "губы легкой мягкости синие."
-                "Cлияние холстов - погружение в транс ",
-
-        }
-    }
-    return render(request, 'main/about.html', Ff)
+    Ff =Articles.objects.order_by('-date')
+    return render(request, 'main/about.html', {'Ff': Ff})
 
